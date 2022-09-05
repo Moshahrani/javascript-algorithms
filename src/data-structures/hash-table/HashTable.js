2193dbf0aa1015c6abec110daa1fee931b49495c
Moshfeq <moshfeqshahrani@gmail.com>
2022-09-04 23:57:01 -0700

    first commit

--- src/data-structures/hash-table/HashTable.js
+++ src/data-structures/hash-table/HashTable.js
@@ -1,8 +1,11 @@
 import LinkedList from '../linked-list/LinkedList';
 
 // Hash table size directly affects on the number of collisions.
+
 // The bigger the hash table size the less collisions you'll get.
+
 // For demonstrating purposes hash table size is small to show how collisions
+
 // are being handled.
 const defaultHashTableSize = 32;
 
